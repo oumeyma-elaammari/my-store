554da29c5d33f11f142b8bd79d9d1a8e1581e1fc
oumeyma-elaammari <oumeyma.elaammari.ensao@ump.ac.ma>
2025-07-04 22:28:56 +0100

    Ajout

---
+++ src/my-store/productList.jsx
@@ -0,0 +1,12 @@
+function ProductList(){
+    return (
+        <div>
+        <h1>Products List</h1>
+        <p>Here you can find a list of products available in our store.</p>
+        </div>
+    );
+
+
+}
+
+export default ProductList;
